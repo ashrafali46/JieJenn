907c7f7f1eb9d2c2b2edafb7d93eed777a14e851
VasilyGorbunov <vasily.gorbunov@gmail.com>
2022-01-24 14:42:26 +0300

    first commit

---
+++ YoutubeMediaPlayer/app.py
@@ -0,0 +1,4 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QGridLayout, QHBoxLayout, QVBoxLayout
+from PyQt5.QtCore import Qt, QUrl, QEvent
+from PyQt5.QtGui import QIcon
